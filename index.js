878257e322a2fd03664fe50537d00d3538d33cb0
keyuan <keyuan.zhang@samsung.com>
2020-07-28 11:15:35 +0800

    Create base codes

---
+++ index.js
@@ -0,0 +1,11 @@
+const TVWebApp = require('./lib/projectHelper');
+const TizenCM = require('./lib/tizenCertificateManager');
+const SamsungCM = require('./lib/samsungCertificateManager');
+const ProfileManager = require('./lib/profileManager');
+
+module.exports = {
+    TVWebApp,
+    TizenCM,
+    SamsungCM,
+    ProfileManager
+}
